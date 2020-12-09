905eb0efb50ed6f26a26416f96b55dc9d450b902
Margarida <margarida97pt@gmail.com>
2020-12-09 18:18:47 +0000

    day5

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const session = require("express-session");
 require("./configs/passport");
 
 mongoose
-  .connect("mongodb://localhost/mimi-be", { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
