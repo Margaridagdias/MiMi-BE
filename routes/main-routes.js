6b0db35cef6a57dae59ab90b7a7d00c0b987f15e
Margarida <margarida97pt@gmail.com>
2020-12-11 14:22:58 +0000

    final version project 3 MiMi

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -4,16 +4,15 @@ const fileUpload = require('../configs/cloudinary');
 const Post = require('../models/post-model');
 const axios = require('axios')
 const path = require('path')
-
+/*
 let config = {
   method: 'get',
   url: 'https://api.twitter.com/1.1/trends/place.json?id=23424925',
   headers: { 
-    'Authorization': 'OAuth oauth_consumer_key="hJfLjnVfEVDo1XbWx62HrSjDT",oauth_token="186525972-aSvHvxHXxSNgXcP03NZHxzYwXxqdzRj68mjvZxaj",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1607600065",oauth_nonce="zm9nkPi57mL",oauth_version="1.0",oauth_signature="m5wFy5mc0ysZhyX4lEsb26qyipk%3D"', 
-    'Cookie': 'personalization_id="v1_hBeKSi6kOsEh4Jt4wK8Cng=="; guest_id=v1%3A160708231310251882; lang=pt'
+    'Authorization': 'OAuth oauth_consumer_key="V4ye2SZS1WQb6ZUy8MyxS0L8q",oauth_token="186525972-XRWHyisNLsWq4kb81wrFdK547KE7HsUjpkAjrvKf",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1607691255",oauth_nonce="xdFkx74PGGu",oauth_version="1.0",oauth_signature="ua8VK7cZprp1XxGcUYLJ3%2BjAms4%3D"'
   }
 };
-
+*/
 
 /* GET home page */
 router.get('/main', (req, res, next) => {
@@ -24,16 +23,23 @@ router.get('/main', (req, res, next) => {
  
 });
 
-router.get('/trends', (req, res, next) => {
-  axios(config)
-  .then(function (response) {
-    console.log(response.data)
-   res.json(response.data[0].trends);
-  })
-  .catch(function (error) {
-    res.json(error)
-  });
-})
+// router.get('/trends', (req, res, next) => {
+//   console.log('route was called')
+//   axios({
+//     method: 'get',
+//     url: 'https://api.twitter.com/1.1/trends/place.json?id=23424925',
+//     headers: { 
+//       'Authorization': 'OAuth oauth_consumer_key="V4ye2SZS1WQb6ZUy8MyxS0L8q",oauth_token="186525972-XRWHyisNLsWq4kb81wrFdK547KE7HsUjpkAjrvKf",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1607691255",oauth_nonce="xdFkx74PGGu",oauth_version="1.0",oauth_signature="ua8VK7cZprp1XxGcUYLJ3%2BjAms4%3D"'
+//     }
+//   })
+//   .then( (response) => {
+//     console.log(response.data)
+//    res.json(response.data[0].trends);
+//   })
+//   .catch( (error) => {
+//     res.json(error)
+//   });
+// })
 
 router.all("*", (req,res,next) =>{
   if (req.originalUrl.startsWith('/api')) {
